1af658d02b3d9198dbb887cabd82cfd12c46b893
Jannes <Jannes.volkens@haw-hamburg.de>
2019-12-06 10:40:24 +0100

    examples/IPToCan: Initial commit

---
+++ examples/IPToCan/main.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2019 HAW-Hamburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       IP to CAN test application
+ *
+ * @author      Jannes Volkens <jannes.volkens@haw-hamburg.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "thread.h"
+#include "shell.h"
+#include "shell_commands.h"
+
+#ifdef MODULE_NETIF
+#include "net/gnrc/pktdump.h"
+#include "net/gnrc.h"
+#endif
+
+int main(void)
+{
+#ifdef MODULE_NETIF
+    gnrc_netreg_entry_t dump = GNRC_NETREG_ENTRY_INIT_PID(GNRC_NETREG_DEMUX_CTX_ALL,
+                                                          gnrc_pktdump_pid);
+    gnrc_netreg_register(GNRC_NETTYPE_UNDEF, &dump);
+#endif
+
+    (void) puts("Welcome to RIOT!");
+
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+    return 0;
+}
