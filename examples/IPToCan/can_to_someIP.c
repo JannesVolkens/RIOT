256a21406a25ec9dc3c5ee106d25841111b7782e
Jannes <Jannes.volkens@haw-hamburg.de>
2019-12-19 14:59:49 +0100

    examples/IPToCan: First implementation of SOME/IP header

--- examples/IPToCan/can_to_someIP.c
+++ examples/IPToCan/can_to_someIP.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "can/can.h"
 
 // Request ID, Protocol version, Interface version, Message type, Return Code
@@ -11,16 +12,52 @@ static uint32_t get_length(uint8_t dlc)
       return dlc + HEADERSIZE;
 }
 
+static void set_ID(uint8_t *buf, uint32_t id)
+{
+    buf[3] = (id & 0x000000ff);
+    buf[2] = (id & 0x0000ff00) >> 8;
+    buf[1] = (id & 0x00ff0000) >> 16;
+    buf[0] = (id & 0xff000000) >> 24;
+}
+
+static void set_length(uint8_t *buf, uint32_t length)
+{
+    buf[7] = (length & 0x000000ff);
+    buf[6] = (length & 0x0000ff00) >> 8;
+    buf[5] = (length & 0x00ff0000) >> 16;
+    buf[4] = (length & 0xff000000) >> 24;
+}
+
+static void set_client_id(uint8_t *buf, uint16_t id)
+{
+    buf[9] = (id & 0x00ff);
+    buf[8] = (id & 0xff00) >> 8;
+}
+
+static void fill_payload(uint8_t *buf, const uint8_t *data, uint8_t dlc)
+{
+    for (int i = 16; i < (16 + dlc); i++) {
+        buf[i] = data[i - 16];
+    }
+}
+
 void can_to_someIP(const struct can_frame *frame)
 {
-    printf("ID: %lx\nDLC: %d\n", frame->can_id, frame->can_dlc);
-    for (int i = 0; i < frame->can_dlc; i++) {
-        printf("DATA[%d]: %x\n", i, frame->data[i]);
-    }
-
     uint8_t buf[BUFSIZE(get_length(frame->can_dlc))];
+    memset(buf, 0, sizeof(buf));
 
-    buf[0] = frame->can_id;
+    set_ID(buf, frame->can_id);
+    set_length(buf, get_length(frame->can_dlc));
+    set_client_id(buf, 0xabcd);
+    //Protoco, Interface, Message type, Return code
+    buf[12] = 0x01;
+    buf[13] = 0x00;
+    buf[14] = 0x01;
+    buf[15] = 0x00;
 
-    printf("SIZE: %d\n", sizeof(buf));
+    fill_payload(buf, frame->data, frame->can_dlc);
+
+    for (size_t i = 0; i < sizeof(buf); i++) {
+        printf("BUF[%d]: %x\n", i, buf[i]);
+    }
 }
